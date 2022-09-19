38b311eaa732fe03db760f2ca2fb46b13a5ab8a0
Kent Henneuse <kent@convoy.com>
2022-09-19 16:34:12 -0700

    Add database connection
    Connection is now built into the server startup.  This allows for the
    controllers to call into the database despite that being the wrong
    place for the logic.  It should be in another layer that is just for
    data exchange with the database.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response } from 'express';
+import { createConnection } from 'typeorm';
 
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+createConnection()
+  .then(async () => {
 
-app.listen(port, () => {
-  console.log(`⚡️[loan-service]: Server is running at http://localhost:${port}`);
-});
+    app.use(express.json());
+    app.get('/', (_req: Request, res: Response) => {
+      res.send('Express + TypeScript Server');
+    });
+
+    app.listen(port, () => {
+      console.log(`⚡️[loan-service]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
