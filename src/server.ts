715f6c033443188f1623315d6dca3a02ed297abe
Kent Henneuse <kent@henneuse.org>
2022-09-28 22:40:54 -0700

    Revert "Add test for the UserController"
    This reverts commit f239a3eda402d6e43da2ad8b03ccb5f20b284acc.

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,21 @@ import express, { Express, Request, Response } from 'express';
 import { createConnection } from 'typeorm';
 import { LoanApplicationController, UserController } from './controllers';
 
-export const app: Express = express();
+const app: Express = express();
 const port = 3000;
-
-const userController = new UserController();
-const loanApplicationController = new LoanApplicationController();
-
-app.use(express.json());
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-app.use('/users', userController.router);
-app.use('/applications', loanApplicationController.router);
 
 createConnection()
   .then(async () => {
+    const userController = new UserController();
+    const loanApplicationController = new LoanApplicationController();
+
+    app.use(express.json());
+    app.get('/', (_req: Request, res: Response) => {
+      res.send('Express + TypeScript Server');
+    });
+    app.use('/users', userController.router);
+    app.use('/applications', loanApplicationController.router);
+
     app.listen(port, () => {
       console.log(`⚡️[loan-service]: Server is running at http://localhost:${port}`);
     });
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,29 +1,18 @@
-/* eslint-disable @typescript-eslint/no-use-before-define */
 import { getRepository } from 'typeorm';
 import { inspect } from 'util';
 import { v4 as uuidV4 } from 'uuid';
-import { User as UserEntity } from '../entities/User';
-import { UserDTO } from './dtos';
+import { User } from '../entities/User';
 
-export async function createUser(name: string, id = uuidV4()): Promise<UserDTO> {
-  const result = await getRepository(UserEntity).save({ id, name });
+export async function createUser(name: string, id = uuidV4()): Promise<User> {
+  const result = await getRepository(User).save({ id, name });
   console.log(`User created ${inspect(result)}`);
-  return convertToUserDTO(result);
+  return result;
 }
 
-export async function getAllUsers(): Promise<UserDTO[]> {
-  const entities = await getRepository(UserEntity).find();
-  return entities.map(convertToUserDTO);
+export async function getAllUsers(): Promise<User[]> {
+  return getRepository(User).find();
 }
 
-export async function getUser(id:string): Promise<UserDTO | null> {
-  const entity = await getRepository(UserEntity).findOne(id);
-  return entity ? convertToUserDTO(entity) : null;
-}
-
-function convertToUserDTO(entity: UserEntity): UserDTO {
-  return {
-    id: entity.id,
-    name: entity.name,
-  };
+export async function getUser(id:string): Promise<User | undefined> {
+  return getRepository(User).findOne(id);
 }
